d3542e52b8908597834a50489b31a6f4eef4e198
KenyTuan <vothanhtuan098@gmail.com>
2024-05-15 21:40:10 +0700

    init project

---
+++ src/main/java/com/test/tutipet/TutipetApplication.java
@@ -0,0 +1,13 @@
+package com.test.tutipet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutipetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutipetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/tutipet/TutipetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tutipet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutipetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
